bb506f89b9e58edf8de5225573e369cece76d8f0
unknown <xuberswork@gmail.com>
2024-12-30 21:27:43 +0530

    Initial Setup for Fast API

---
+++ Books Project/books.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
+
+"""
+%20 means space.
+
+Always dynamic path parameters will be at last 
+order of reading API in FastAPI :
+                                 Static > Dynamic
+"""
+
+@app.get("/books/{title_name}")
+async def get_book(title_name: str):
+    for book in BOOKS:
+        if book['title'].casefold() == title_name.casefold():
+            return book
+    return {'message': 'Book not found'}
