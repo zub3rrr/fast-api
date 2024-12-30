27ba62e68bb74dc66972a889d407a125578b66ba
unknown <xuberswork@gmail.com>
2024-12-31 00:21:29 +0530

    Added few more endpoints and finished

--- Books Project/books.py
+++ Books Project/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body
 
 app = FastAPI()
 
@@ -26,9 +26,56 @@ order of reading API in FastAPI :
                                  Static > Dynamic
 """
 
+
+# Dynamic path parameters - or Dynamic Parameters
 @app.get("/books/{title_name}")
 async def get_book(title_name: str):
     for book in BOOKS:
         if book['title'].casefold() == title_name.casefold():
             return book
+    return {'message': 'Book not found'}
+
+
+# Query Parameters - ? -- http://<local_path>/books/?category=math
+"""
+Here what we will do is that after /books/{author_name}/ we will add ?category=science
+to filter out categories from data.
+"""
+
+@app.get("/books/{author_name}/")
+async def get_books_by_author_category(author_name:str, category: str):
+    books = []
+    for book in BOOKS:
+        if book['author'].casefold() == author_name.casefold() and book['category'].casefold() == category.casefold():
+            books.append(book)
+    if books:
+        return books
+    else:
+        return {'message': f'Books not found for {author_name}'}
+    
+
+# post - method
+
+@app.post("/books/create_book")
+async def create_book(create_book=Body()):
+    BOOKS.append(create_book)
+    return BOOKS
+
+
+# Put Request - update Request
+@app.put("/books/update_book")
+async def update_book(update_book=Body()):
+    for book in BOOKS:
+        if book['title'].casefold() == update_book['title'].casefold():
+            book.update(update_book)
+            return {'message': 'Book updated successfully'}
+    return {'message': 'Book not found'}
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for book in BOOKS:
+        if book['title'].casefold() == book_title.casefold():
+            BOOKS.remove(book)
+            return {'message': 'Book deleted successfully'}
     return {'message': 'Book not found'}
