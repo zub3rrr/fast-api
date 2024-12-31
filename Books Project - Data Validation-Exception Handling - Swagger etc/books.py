dc8f904850da13d3a1dd415ef1fe06b5345be64f
unknown <xuberswork@gmail.com>
2024-12-31 16:12:58 +0530

    Books Project updated scope

---
+++ Books Project - Data Validation-Exception Handling - Swagger etc/books.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI , Body
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
