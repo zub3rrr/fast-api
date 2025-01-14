d370ea06702f866975526f8ad7046ab55722c72f
unknown <xuberswork@gmail.com>
2025-01-14 15:42:09 +0530

    Added Validation for get req , HTTP Status Code

--- Books Project - Data Validation-Exception Handling - Swagger etc/books.py
+++ Books Project - Data Validation-Exception Handling - Swagger etc/books.py
@@ -1,19 +1,12 @@
-"""
-Scope of this project is to understand the
-1. Data validation at post request
-2. Exception handling
-"""
 from typing import Optional
-from fastapi import FastAPI , Body
+from fastapi import FastAPI, Path, Query, HTTPException, Body
 from pydantic import BaseModel, Field
-
+from starlette import status
 
 app = FastAPI()
 
+
 class Book:
-    """
-    A simple book model/class to create record with fields for id, title, author, description, rating, and published_date.
-    """
     id: int
     title: str
     author: str
@@ -31,9 +24,6 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    """
-    A class to validate the request body for creating a new book.
-    """
     id: Optional[int] = Field(description='ID is not needed on create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
@@ -52,11 +42,8 @@ class BookRequest(BaseModel):
             }
         }
     }
+        
 
-"""
-gt = greater_than
-lt = less_than
-"""
 
 
 BOOKS = [
@@ -69,22 +56,21 @@ BOOKS = [
 ]
 
 
-
-
-@app.get("/books")
-async def get_all_books():
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{book_id}")
-async def read_book(book_id: int):
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
-        
+    raise HTTPException(status_code=404, detail='Item not found')
 
-@app.get("/books/", )
-async def read_book_by_rating(book_rating: int):
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -93,43 +79,44 @@ async def read_book_by_rating(book_rating: int):
 
 
 
-"""
-Now on create book request we will implement Data Validation.
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
-"""
 
-@app.post("/create-books")
-async def create_books(book_request:BookRequest):
-    try:
-        new_book = Book(**book_request.model_dump())
-        BOOKS.append(find_book_id(new_book))
-        return {"message": f"Book created successfully."}
-    except Exception as e:
-        return {"error": str(e)}
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
 
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
     book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
-    
-    return {"message": f"Book updated successfully."}
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.delete("/books/{book_id}")
-async def delete_book(book_id: int ):
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
     book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
             book_changed = True
             break
-    if book_changed:
-        return {"message": f"Book deleted successfully."}
-
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
